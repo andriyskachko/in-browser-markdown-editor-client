2ebcf1300b27f5c593a6f2a0f98cd5b3dfd9947e
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-12-03 01:01:58 +0200

    DocumentListItem and styles

--- src/components/DocumentListItem.tsx
+++ src/components/DocumentListItem.tsx
@@ -1,23 +1,40 @@
 import IDocument from '@/shared/IDocument'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { format } from 'date-fns'
+import '@sass/components/DocumentListItem.scss'
+
+interface IDocumentListItemProps {
+  document: IDocument
+  handleClick: (id?: string) => void
+}
 
 function dateToShortFormat(date: string) {
   const parseDate = new Date(date)
   const formatting = 'dd MMMM yyyy'
   const formatted = format(parseDate, formatting)
-
   return formatted
 }
 
-function DocumentListItem({ document }: { document: IDocument }) {
-  const { name, createdAt } = document
+function DocumentListItem({ document, handleClick }: IDocumentListItemProps) {
+  const { _id, name, createdAt } = document
   const formattedDate = dateToShortFormat(createdAt)
 
   return (
-    <div className="DocumentListItem">
-      <span>{name}</span>
-      <span>{formattedDate}</span>
-    </div>
+    <li
+      className="DocumentListItem"
+      tabIndex={0}
+      onClick={() => handleClick(_id)}
+    >
+      <FontAwesomeIcon icon="file" />
+      <div className="DocumentListItem__info">
+        <span className="DocumentListItem__date text-body-S clr-heading-body">
+          {formattedDate}
+        </span>
+        <span className="DocumentListItem__name text-heading-M clr-heading">
+          {name}
+        </span>
+      </div>
+    </li>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ThemeContextProvider from './context/ThemeContext'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSun, faFile } from '@fortawesome/free-solid-svg-icons'
 import App from './App'
 import './main.scss'
 
-library.add(faMoon, faSun)
+library.add(faMoon, faSun, faFile)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
