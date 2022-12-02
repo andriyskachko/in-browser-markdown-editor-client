68128ffb6b5bb49c7b032b488ae26b3e7d66114b
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-12-03 01:01:42 +0200

    added props to documents list item

--- src/components/DocumentsList.tsx
+++ src/components/DocumentsList.tsx
@@ -1,20 +1,32 @@
 import IDocument from '@/shared/IDocument'
 import DocumentListItem from '@/components/DocumentListItem'
 
-interface DocumentsListProps {
+interface IDocumentsListProps {
   documents: IDocument[]
   error?: Error
   isLoading: boolean
 }
 
-function DocumentsList({ documents, error, isLoading }: DocumentsListProps) {
+function DocumentsList({ documents, error, isLoading }: IDocumentsListProps) {
+  const handleClick = (id?: string) => {
+    console.log(id)
+  }
+
   return (
     <div>
       {isLoading && <h1>Loading..</h1>}
       {error && <h1>Error</h1>}
-      {documents.map((document) => {
-        return <DocumentListItem document={document} />
-      })}
+      <ul>
+        {documents.map((document) => {
+          return (
+            <DocumentListItem
+              document={document}
+              key={document._id}
+              handleClick={handleClick}
+            />
+          )
+        })}
+      </ul>
     </div>
   )
 }
