78173d48f3f4efc6d83a0e64e92ddcb21dd4b940
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-12-04 01:12:07 +0200

    working on Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,20 @@
+import DocumentsList from '@/components/DocumentsList'
+import '@sass/components/Sidebar.scss'
+
+function Sidebar({ isOpen }: { isOpen: boolean }) {
+  return (
+    <div className={`Sidebar ${isOpen ? '' : 'hidden'}`}>
+      <h1 className="text-heading-S Sidebar__header">MY DOCUMENTS</h1>
+      <button
+        className="button button--primary"
+        onClick={() => console.log('New Document Create Clicked')}
+      >
+        + New Document
+      </button>
+
+      <DocumentsList />
+    </div>
+  )
+}
+
+export default Sidebar
